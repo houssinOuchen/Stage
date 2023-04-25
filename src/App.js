34e2f304875f7be21f763029615560f7c2d3b0e9
houssaineOuchen <houssin112002@gmail.com>
2023-04-25 19:11:28 +0000

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+import GetFlash from "./Redux/Components/GetFlash";
+import VenteJrnl from "./Redux/Components/VenteJrnl";
+import GetStock from "./Redux/Components/Stock";
+import { Link, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
+import './NavBar.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default function App() {
+
+    return(
+    <>
+        <Provider store={store}>
+            <div className="parent">
+                <div className="navbar-container">
+                    <NavBar />
+                </div>
+                <div className="content-container">
+                    <Routes>
+                        <Route path="/" element={<GetFlash />} />
+                        <Route path="/GetFlash" element={<GetFlash />} />
+                        <Route path="/VenteJrnl" element={<VenteJrnl />} />
+                        <Route path="/GetStock" element={<GetStock />} />
+                    </Routes>
+                </div>
+            </div>
+            
+        </Provider>
+
+    </>
+    )
 }
 
-export default App;
+
